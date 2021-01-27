7c34a11d8184f47cbce031b283824b3269ab1f2c
stonelym <stonelym@gmail.com>
2021-01-27 00:17:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from V2
+          Hello from V3
         </p>
         <a
           className="App-link"
