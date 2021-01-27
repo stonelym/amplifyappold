c24f0b1577acf9ec5f0a5454a48518b242e1d2b9
stonelym <stonelym@gmail.com>
2021-01-26 23:50:30 -0500

    V2.1
    spelling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellp from V2
+          Hello from V2
         </p>
         <a
           className="App-link"
